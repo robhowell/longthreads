ee87d248f6d12786f423920ebdbf1de48c80a672
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-15 23:40:43 +0100

    Add TypeScript path support to Storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,6 @@
+const path = require('path');
+const TsconfigPathsPlugin = require('tsconfig-paths-webpack-plugin');
+
 module.exports = {
   "stories": [
     "../src/**/*.stories.mdx",
@@ -6,5 +9,18 @@ module.exports = {
   "addons": [
     "@storybook/addon-links",
     "@storybook/addon-essentials"
-  ]
+  ],
+  webpackFinal: async (config, {configType}) => {
+    const tsPlugin = new TsconfigPathsPlugin({
+      configFile: path.resolve(__dirname, '../tsconfig.json')
+    })
+
+    if (config.resolve.plugins) {
+      config.resolve.plugins.push(tsPlugin);
+    } else {
+      config.resolve.plugins = [tsPlugin];
+    }
+
+    return config;
+  },
 }

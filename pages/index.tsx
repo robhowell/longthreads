fc873c8eb524037a6ea742d03aceae780a24e823
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-15 21:40:03 +0100

    Setup Sanitize.css - closes #10

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,30 @@
 import React from 'react';
 import Head from 'next/head';
+import styled from 'styled-components';
+
+const Main = styled.main`
+  margin: 0 auto;
+  max-width: 800px;
+  padding: 20px;
+`;
 
 export default function Home() {
   return (
-    <div>
+    <>
       <Head>
         <title>Long Threads</title>
         <meta name="description" content="A simple todo list app" />
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://unpkg.com/sanitize.css/evergreen.css" rel="stylesheet" />
       </Head>
 
-      <main>
+      <Main>
         <h1>
           Long Threads
         </h1>
 
         Test
-      </main>
-    </div>
+      </Main>
+    </>
   );
 }

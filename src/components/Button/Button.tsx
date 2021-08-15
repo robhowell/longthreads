f89bba28f81173c196595b7835e8d5cc7726e486
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-15 01:12:37 +0100

    Setup Storybook with initial test story

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,12 @@
+import React, {ReactNode} from 'react';
+
+interface ButtonProps {
+  children: ReactNode,
+  type: 'button'|'submit'|'reset'
+}
+
+const Button = ({children, type = 'button'}: ButtonProps) => (
+  <button type={type}>{children}</button>
+);
+
+export default Button;

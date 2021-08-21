f1397a30c3a8585c27ad3a1a7b3e9fa7ed4fcebc
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-21 23:37:08 +0100

    Add BareTextInput component & storybook example

--- src/components/BareTextInput/BareTextInput.js
+++ src/components/BareTextInput/BareTextInput.js
@@ -4,12 +4,16 @@ import styled from "styled-components";
 const StyledTextInput = styled.input`
   border: none;
   background-color: transparent;
+  ${({ $fullWidth }) => $fullWidth && `width: 100%`}
 `;
 
-const BareTextInput = ({ children, onChange }) => (
-  <StyledTextInput type="text" onChange={onChange}>
-    {children}
-  </StyledTextInput>
+const BareTextInput = ({ fullWidth = false, value, onChange }) => (
+  <StyledTextInput
+    type="text"
+    onChange={onChange}
+    value={value}
+    $fullWidth={fullWidth}
+  />
 );
 
 export default BareTextInput;

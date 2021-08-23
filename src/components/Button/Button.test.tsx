6e98d80be89da154cc31a3eebe1290c86efc4fe6
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-23 18:12:39 +0100

    Add extra unit tests for Button component

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -8,4 +8,26 @@ describe("Button", () => {
     render(<Button onClick={() => {}}>Hello Button</Button>);
     expect(screen.getByText("Hello Button")).toBeInTheDocument();
   });
+
+  it("passes through type", () => {
+    render(
+      <Button onClick={() => {}} type="submit">
+        Submit Button
+      </Button>
+    );
+    const submitButton = screen.getByText("Submit Button");
+    expect(submitButton).toBeInTheDocument();
+    expect(submitButton.getAttribute("type")).toBe("submit");
+  });
+
+  it("allows custom element types to be rendered (e.g. <a>)", () => {
+    render(
+      <Button onClick={() => {}} as="a">
+        Link Button
+      </Button>
+    );
+    const linkButton = screen.getByText("Link Button");
+    expect(linkButton).toBeInTheDocument();
+    expect(linkButton.nodeName.toLowerCase()).toBe("a");
+  });
 });

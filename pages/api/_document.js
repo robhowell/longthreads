61bd53f1dc1eb6aae0c6008d301bb141b0bef5e1
Rob Howell <3998397+robhowell@users.noreply.github.com>
2021-08-29 00:46:04 +0100

    Update index to render App component

---
+++ pages/api/_document.js
@@ -0,0 +1,33 @@
+import Document, { Html, Head, Main, NextScript } from "next/document";
+
+class LongThreadsDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <meta name="description" content="A simple todo list app" />
+          <link rel="icon" href="/favicon.ico" />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/css?family=Noto+Sans+JP:300,400,500,700&display=swap"
+          />
+          <link
+            rel="stylesheet"
+            href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          />
+          <link
+            href="https://unpkg.com/sanitize.css/evergreen.css"
+            rel="stylesheet"
+          />
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default LongThreadsDocument;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,15 @@
 import React from "react";
 import Head from "next/head";
-import styled from "styled-components";
-
-import { blue, darkGrey } from "~/styles/colors";
-import { spacing5, spacing17 } from "~/styles/spacing";
-
-const Main = styled.main`
-  color: ${darkGrey};
-  margin: 0 auto;
-  max-width: ${spacing17};
-  padding: ${spacing5};
-`;
-
-const PageTitle = styled.h1`
-  color: ${blue};
-  font-size: 1.5rem;
-`;
+import App from "~/components/App/App";
 
 export default function Home() {
   return (
     <>
       <Head>
         <title>Long Threads</title>
-        <meta name="description" content="A simple todo list app" />
-        <link rel="icon" href="/favicon.ico" />
-        <link
-          href="https://unpkg.com/sanitize.css/evergreen.css"
-          rel="stylesheet"
-        />
       </Head>
 
-      <Main>
-        <PageTitle>Long Threads</PageTitle>
-        Test
-      </Main>
+      <App />
     </>
   );
 }
